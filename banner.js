d11de79c48af4d879cc2ddfbe09275135f88e089
DinaraDervel <dinaradervel@gmail.com>
2024-01-18 11:03:06 -0600

    fix bugs

--- banner.js
+++ banner.js
@@ -1,167 +1,167 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const banner = document.querySelector('.banner');
-  const overlay = document.querySelector('.overlay');
-  const closeButton = document.getElementById('close-button');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const banner = document.querySelector('.banner');
+//   const overlay = document.querySelector('.overlay');
+//   const closeButton = document.getElementById('close-button');
 
-  const saveScrollPosition = function () {
-    localStorage.setItem('scrollPosition', window.pageYOffset);
-  };
+//   const saveScrollPosition = function () {
+//     localStorage.setItem('scrollPosition', window.pageYOffset);
+//   };
 
-  const restoreScrollPosition = function () {
-    const scrollPosition = localStorage.getItem('scrollPosition');
-    if (scrollPosition) {
-      window.scrollTo(0, scrollPosition);
-    }
-  };
+//   const restoreScrollPosition = function () {
+//     const scrollPosition = localStorage.getItem('scrollPosition');
+//     if (scrollPosition) {
+//       window.scrollTo(0, scrollPosition);
+//     }
+//   };
 
-  const showBanner = function () {
-    banner.style.display = 'block';
-    overlay.style.display = 'block';
-  };
+//   const showBanner = function () {
+//     banner.style.display = 'block';
+//     overlay.style.display = 'block';
+//   };
 
-setInterval(showBanner, 180000);
-
-
-  const closeBanner = function () {
-    banner.style.display = 'none';
-    overlay.style.display = 'none';
-  };
-
-  // Закрываем баннер и оверлей пр и щелчке вне баннера
-  overlay.addEventListener('click', function (e) {
-    if (e.target === overlay) {
-      closeBanner();
-    }
-  });
-
-  closeButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    closeBanner();
-    saveScrollPosition();
-  });
-
-  const phoneInputButton = document.querySelector('.phone-input-button');
-  phoneInputButton.addEventListener('click', function () {
-    const phoneInput = document.querySelector('.phone-input-input');
-    const phoneNumber = phoneInput.value;
-  });
-
-  const fortune = document.querySelector('.fortune');
-  const fortuneOverlay = document.querySelector('.fortune-overlay');
-  const fortuneCloseButton = document.getElementById('fortune-close-button');
+// setInterval(showBanner, 180000);
 
 
-  const callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        showFortune(); // Вызов функции showFortune, когда пользователь долистывает до середины страницы
-      }
-    });
-  };
+//   const closeBanner = function () {
+//     banner.style.display = 'none';
+//     overlay.style.display = 'none';
+//   };
 
-  const target = document.querySelector('.wrapper-map');
+//   // Закрываем баннер и оверлей пр и щелчке вне баннера
+//   overlay.addEventListener('click', function (e) {
+//     if (e.target === overlay) {
+//       closeBanner();
+//     }
+//   });
 
-  const observer = new IntersectionObserver(callback, {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.5,
-  });
+//   closeButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     closeBanner();
+//     saveScrollPosition();
+//   });
+
+//   const phoneInputButton = document.querySelector('.phone-input-button');
+//   phoneInputButton.addEventListener('click', function () {
+//     const phoneInput = document.querySelector('.phone-input-input');
+//     const phoneNumber = phoneInput.value;
+//   });
+
+//   const fortune = document.querySelector('.fortune');
+//   const fortuneOverlay = document.querySelector('.fortune-overlay');
+//   const fortuneCloseButton = document.getElementById('fortune-close-button');
 
 
-  observer.observe(target);
+//   const callback = (entries, observer) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         showFortune(); // Вызов функции showFortune, когда пользователь долистывает до середины страницы
+//       }
+//     });
+//   };
 
-  const showFortune = function () {
-    fortune.style.display = 'block';
-    fortuneOverlay.style.display = 'block';
-  };
+//   const target = document.querySelector('.wrapper-map');
 
-  const closeFortune = function () {
-    fortune.style.display = 'none';
-    fortuneOverlay.style.display = 'none';
-  };
+//   const observer = new IntersectionObserver(callback, {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.5,
+//   });
 
-  // Закрываем баннер удачи и оверлей при щелчке вне баннера
-  fortuneOverlay.addEventListener('click', function (e) {
-    if (e.target === fortuneOverlay) {
-      closeFortune();
-    }
-  });
 
-  fortuneCloseButton.addEventListener('click', function () {
-    closeFortune();
-    saveScrollPosition();
-  });
+//   observer.observe(target);
 
-  const wheel = document.querySelector('.wheel');
-  const fortuneArrow = document.getElementById('fortune-arrow');
-  const fortuneResultInput = document.getElementById('fortune-result');
+//   const showFortune = function () {
+//     fortune.style.display = 'block';
+//     fortuneOverlay.style.display = 'block';
+//   };
 
-  function getResultMessage(randomDegrees) {
-    switch (randomDegrees) {
-      case 1:
-        return "Бесплатная 3D диагностика зубов";
-      case 2:
-        return "Портативный ирригатор в подарок";
-      case 3:
-        return "Скидка 25 000 рублей на имплантацию";
-      case 4:
-        return "Комплекс по уходу за полостью рта";
-      case 5:
-        return "Консультация стоматолога бесплатно";
-      case 6:
-        return "Имплантация All-on-4 в подарок";
-      default:
-        return "Нет таких значений";
-    }
-  }
+//   const closeFortune = function () {
+//     fortune.style.display = 'none';
+//     fortuneOverlay.style.display = 'none';
+//   };
 
-  function spinWheel() {
-    const randomDegrees = Math.floor(Math.random() * 6) + 1;
+//   // Закрываем баннер удачи и оверлей при щелчке вне баннера
+//   fortuneOverlay.addEventListener('click', function (e) {
+//     if (e.target === fortuneOverlay) {
+//       closeFortune();
+//     }
+//   });
 
-    wheel.style.animationName = `wheel-rotate-${randomDegrees}`;
-    wheel.style.display = "block";
+//   fortuneCloseButton.addEventListener('click', function () {
+//     closeFortune();
+//     saveScrollPosition();
+//   });
 
-    switch (randomDegrees) {
-      case 1:
-        fortuneResultInput.value = "Бесплатная 3D диагностика зубов";
-        break;
-      case 2:
-        fortuneResultInput.value = "Портативный ирригатор в подарок";
-        break;
-      case 3:
-        fortuneResultInput.value = "Скидка 25 000 рублей на имплантацию";
-        break;
-      case 4:
-        fortuneResultInput.value = "Комплекс по уходу за полостью рта";
-        break;
-      case 5:
-        fortuneResultInput.value = "Консультация стоматолога бесплатно";
-        break;
-      case 6:
-        fortuneResultInput.value = "Имплантация All-on-4 в подарок";
-        break;
-      default:
-        fortuneResultInput.value = "Нет таких значений";
-    }
+//   const wheel = document.querySelector('.wheel');
+//   const fortuneArrow = document.getElementById('fortune-arrow');
+//   const fortuneResultInput = document.getElementById('fortune-result');
 
-    setTimeout(function() {
-      fortuneResultInput.value = getResultMessage(randomDegrees);
-    }, 900000);
-  }
+//   function getResultMessage(randomDegrees) {
+//     switch (randomDegrees) {
+//       case 1:
+//         return "Бесплатная 3D диагностика зубов";
+//       case 2:
+//         return "Портативный ирригатор в подарок";
+//       case 3:
+//         return "Скидка 25 000 рублей на имплантацию";
+//       case 4:
+//         return "Комплекс по уходу за полостью рта";
+//       case 5:
+//         return "Консультация стоматолога бесплатно";
+//       case 6:
+//         return "Имплантация All-on-4 в подарок";
+//       default:
+//         return "Нет таких значений";
+//     }
+//   }
 
-    fortuneArrow.addEventListener('click', spinWheel, { once: true });
+//   function spinWheel() {
+//     const randomDegrees = Math.floor(Math.random() * 6) + 1;
 
-    function updateDate() {
-      const dateElement = document.getElementById('date');
-      const currentDate = new Date();
-      currentDate.setDate(currentDate.getDate() + 3);
-      const options = { day: 'numeric', month: 'long' };
-      const formattedDate = currentDate.toLocaleDateString('ru-RU', options);
-      dateElement.textContent = formattedDate;
-    }
-    setInterval(updateDate, 10000000);
+//     wheel.style.animationName = `wheel-rotate-${randomDegrees}`;
+//     wheel.style.display = "block";
 
-    window.addEventListener('load', restoreScrollPosition);
+//     switch (randomDegrees) {
+//       case 1:
+//         fortuneResultInput.value = "Бесплатная 3D диагностика зубов";
+//         break;
+//       case 2:
+//         fortuneResultInput.value = "Портативный ирригатор в подарок";
+//         break;
+//       case 3:
+//         fortuneResultInput.value = "Скидка 25 000 рублей на имплантацию";
+//         break;
+//       case 4:
+//         fortuneResultInput.value = "Комплекс по уходу за полостью рта";
+//         break;
+//       case 5:
+//         fortuneResultInput.value = "Консультация стоматолога бесплатно";
+//         break;
+//       case 6:
+//         fortuneResultInput.value = "Имплантация All-on-4 в подарок";
+//         break;
+//       default:
+//         fortuneResultInput.value = "Нет таких значений";
+//     }
 
-  }
-);
+//     setTimeout(function() {
+//       fortuneResultInput.value = getResultMessage(randomDegrees);
+//     }, 900000);
+//   }
+
+//     fortuneArrow.addEventListener('click', spinWheel, { once: true });
+
+//     function updateDate() {
+//       const dateElement = document.getElementById('date');
+//       const currentDate = new Date();
+//       currentDate.setDate(currentDate.getDate() + 3);
+//       const options = { day: 'numeric', month: 'long' };
+//       const formattedDate = currentDate.toLocaleDateString('ru-RU', options);
+//       dateElement.textContent = formattedDate;
+//     }
+//     setInterval(updateDate, 10000000);
+
+//     window.addEventListener('load', restoreScrollPosition);
+
+//   }
+// );
